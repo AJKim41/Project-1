c35ef5a4c1f05ca44a1377e7df29d86d4c2bd73f
ajkim41 <ajkim41@gmail.com>
2019-05-08 19:30:17 -0400

    Clean Up (incomplete)

--- public/app.js
+++ public/app.js
@@ -1,14 +1,14 @@
-$(function () {
+$(function() {
   var params = {
     // Request parameters
     showStats: "true"
   };
 
-  document.addEventListener("submit", function (event) {
+  document.addEventListener("submit", function(event) {
     event.preventDefault();
     //-------------------------------  Sentiment API -----------------
-    $("#formsubmission").addClass("off")
-    $("#results1").removeClass("off")
+    $("#formsubmission").addClass("off");
+    $("#results1").removeClass("off");
 
     var lang = document.getElementById("language").value;
     var id = document.getElementById("id").value;
@@ -21,7 +21,7 @@ $(function () {
       url:
         "https://eastus.api.cognitive.microsoft.com/text/analytics/v2.0/sentiment?" +
         $.param(params),
-      beforeSend: function (xhrObj) {
+      beforeSend: function(xhrObj) {
         // Request headers
         xhrObj.setRequestHeader("Content-Type", "application/json");
         xhrObj.setRequestHeader(
@@ -33,7 +33,7 @@ $(function () {
       // Request body
       data: `{ "documents": [${userData}] }`
     })
-      .then(function (response) {
+      .then(function(response) {
         var data = response;
         var score = Math.floor(data.documents[0].score * 100);
         console.log(data);
@@ -51,10 +51,10 @@ $(function () {
           $(".sad-frame").addClass("active");
         }
       })
-      .done(function (data) {
+      .done(function(data) {
         console.log("success");
       })
-      .fail(function () {
+      .fail(function() {
         console.log("error");
       });
 
@@ -70,7 +70,7 @@ $(function () {
       url:
         "https://eastus.api.cognitive.microsoft.com/text/analytics/v2.0/keyPhrases?" +
         $.param(params),
-      beforeSend: function (xhrObj) {
+      beforeSend: function(xhrObj) {
         // Request headers
         xhrObj.setRequestHeader("Content-Type", "application/json");
         xhrObj.setRequestHeader(
@@ -82,170 +82,82 @@ $(function () {
       // Request body
       data: `{ "documents": [${userData}] }`
     })
-      .then(function (response) {
+      .then(function(response) {
         var data = response;
         console.log(data);
         console.log(data.documents[0].keyPhrases[0]);
         var keyPhrase = data.documents[0].keyPhrases[0];
         console.log(data);
         if (keyPhrase === undefined) {
-          $("#key-phrase").html("No Key Phrase")
+          $("#key-phrase").html("No Key Phrase");
         } else {
           $("#key-phrase").html(data.documents[0].keyPhrases[0]);
 
           websterAW(keyPhrase);
         }
-
       })
-      .done(function (data) {
+      .done(function(data) {
         // alert("success");
       })
-      .then(function (response) {
+      .then(function(response) {
         var data = response;
 
         console.log(data.documents[0].keyPhrases);
         $(".keyphrase").html(data.documents[0].keyPhrases);
       })
-      .done(function (data) {
+      .done(function(data) {
         console.log("success");
       })
-      .fail(function () {
+      .fail(function() {
         console.log("error");
       });
-    $('#userInput').val("");
-    $('#searchForm').removeClass("col-md-12");
-    $('#searchForm').addClass("col-md-6")
-    $("#information").attr('style', 'display:block');
-    console.log(keyPhrase)
+    $("#userInput").val("");
+    $("#searchForm").removeClass("col-md-12");
+    $("#searchForm").addClass("col-md-6");
+    $("#information").attr("style", "display:block");
+    console.log(keyPhrase);
   });
 
   async function websterAW(keyPhrase) {
-    let url = await fetch(`https://dictionaryapi.com/api/v3/references/collegiate/json/${keyPhrase}?key=890e17a2-5dcf-4fbe-9e78-69195869c5a2`);
+    let url = await fetch(
+      `https://dictionaryapi.com/api/v3/references/collegiate/json/${keyPhrase}?key=890e17a2-5dcf-4fbe-9e78-69195869c5a2`
+    );
     let data = await url.json();
     console.log(data);
     $("#key-phrase").html(`${keyPhrase}`);
 
     $("#key-phrase-meaning").html(`${data[0].shortdef}`);
-
-  };
-
-  /* Choose A Face
-  let faceA = document.getElementById("happyFace");
-  let faceB = document.getElementById("thinkingFace");
-  let faceC = document.getElementById("sadFace");
-  let wordScore = document.getElementById("keyS").value;
-
-
-  let chosenFace = function () {
-    if (wordScore >= 80) {
-      chosenFace = faceA;
-    } else if (wordScore <= 60) {
-      chosenFace = faceC;
-    } else {
-      chosenFace = faceB;
-    }
-  };
-
-  let displayFace = function () {
-    document.getElementById("chosenFace").innerHTML($(img.val));
-    console.log(chosenFace);
   }
-  chooseFace();
-  displayFace();
-});
-*/
+
   // 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥
   // // The Firebase SDK is initialized and available here!
   //
-  // firebase.auth().onAuthStateChanged(user => { });
-  // firebase.database().ref('/path/to/ref').on('value', snapshot => { });
-  // firebase.messaging().requestPermission().then(() => { });
-  // firebase.storage().ref('/path/to/ref').getDownloadURL().then(() => { });
+  firebase.auth().onAuthStateChanged(user => {});
+  firebase
+    .database()
+    .ref("/path/to/ref")
+    .on("value", snapshot => {});
+  firebase
+    .messaging()
+    .requestPermission()
+    .then(() => {});
+  firebase
+    .storage()
+    .ref("/path/to/ref")
+    .getDownloadURL()
+    .then(() => {});
 
-  // try {
-  //   let app = firebase.app();
-  //   let features = ["auth", "database", "messaging", "storage"].filter(
-  //     feature => typeof app[feature] === "function"
-  //   );
-  //   document.getElementById(
-  //     "load"
-  //   ).innerHTML = `Firebase SDK loaded with ${features.join(", ")}`;
-  // } catch (e) {
-  //   console.error(e);
-  //   document.getElementById("load").innerHTML =
-  //     "Error loading the Firebase SDK, check the console.";
-  // }
-  // // });
-
-
-  // style
-
-
-
-
-
-
-
-});
-
-var colors = new Array(
-  [228, 133, 181],
-  [129, 43, 155],
-  [58, 119, 233],
-  [228, 133, 181],
-  [129, 43, 155],
-  [58, 119, 233]
-);
-
-var step = 0;
-//color table indices for: 
-// current color left
-// next color left
-// current color right
-// next color right
-var colorIndices = [0, 1, 2, 3];
-
-//transition speed
-var gradientSpeed = 0.002;
-
-function updateGradient() {
-
-  if ($ === undefined) return;
-
-  var c0_0 = colors[colorIndices[0]];
-  var c0_1 = colors[colorIndices[1]];
-  var c1_0 = colors[colorIndices[2]];
-  var c1_1 = colors[colorIndices[3]];
-
-  var istep = 1 - step;
-  var r1 = Math.round(istep * c0_0[0] + step * c0_1[0]);
-  var g1 = Math.round(istep * c0_0[1] + step * c0_1[1]);
-  var b1 = Math.round(istep * c0_0[2] + step * c0_1[2]);
-  var color1 = "rgb(" + r1 + "," + g1 + "," + b1 + ")";
-
-  var r2 = Math.round(istep * c1_0[0] + step * c1_1[0]);
-  var g2 = Math.round(istep * c1_0[1] + step * c1_1[1]);
-  var b2 = Math.round(istep * c1_0[2] + step * c1_1[2]);
-  var color2 = "rgb(" + r2 + "," + g2 + "," + b2 + ")";
-
-  $('#gradient').css({
-    background: "-webkit-gradient(linear, left top, right top, from(" + color1 + "), to(" + color2 + "))"
-  }).css({
-    background: "-moz-linear-gradient(left, " + color1 + " 0%, " + color2 + " 100%)"
-  });
-
-  step += gradientSpeed;
-  if (step >= 1) {
-    step %= 1;
-    colorIndices[0] = colorIndices[1];
-    colorIndices[2] = colorIndices[3];
-
-    //pick two new target color indices
-    //do not pick the same as the current one
-    colorIndices[1] = (colorIndices[1] + Math.floor(1 + Math.random() * (colors.length - 1))) % colors.length;
-    colorIndices[3] = (colorIndices[3] + Math.floor(1 + Math.random() * (colors.length - 1))) % colors.length;
-
+  try {
+    let app = firebase.app();
+    let features = ["auth", "database", "messaging", "storage"].filter(
+      feature => typeof app[feature] === "function"
+    );
+    document.getElementById(
+      "load"
+    ).innerHTML = `Firebase SDK loaded with ${features.join(", ")}`;
+  } catch (e) {
+    console.error(e);
+    document.getElementById("load").innerHTML =
+      "Error loading the Firebase SDK, check the console.";
   }
-}
-
-setInterval(updateGradient, 10);
+});
