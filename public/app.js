defdc6425912914625ae3c9a16971529fdfad9b4
ajkim41 <ajkim41@gmail.com>
2019-05-10 14:14:54 -0400

    Entity Url

--- public/app.js
+++ public/app.js
@@ -15,7 +15,6 @@ $(function() {
     var userInput = document.querySelector("#userInput").value;
     var keyPhrase = document.getElementById("keyPhrases").value;
     var userData = JSON.stringify({ language: lang, id: id, text: userInput });
-    console.log(userData);
 
     $.ajax({
       url:
@@ -36,10 +35,7 @@ $(function() {
       .then(function(response) {
         var data = response;
         var score = Math.floor(data.documents[0].score * 100);
-        console.log(data);
-        console.log(score);
 
-        $("#last-definition").html(`<h3>"${userInput}"</h3>`);
         $("#per-score").html(`${score}%`);
 
         $(".happy-frame").removeClass("active");
@@ -54,10 +50,10 @@ $(function() {
         }
       })
       .done(function(data) {
-        console.log("success");
+        //console.log("success");
       })
       .fail(function() {
-        console.log("error");
+        //console.log("error");
       });
 
     //-------------------------------  Entities API -----------------
@@ -80,28 +76,30 @@ $(function() {
     })
       .then(function(response) {
         var data = response;
-        console.log(data);
-        if (data.documents[0].entities.length > 0) {
-          var entityName = data.documents[0].entities[0].name;
-          var entityUrl = data.documents[0].entities[0].wikipediaUrl;
+        for (var i = 0; data.documents[0].entities.length > i; i++) {
+          var entityName = data.documents[0].entities[i].name;
+          var entityUrl = data.documents[0].entities[i].wikipediaUrl;
           console.log(entityName);
           console.log(entityUrl);
-          setTimeout(
-            $(`#last-definition:contains("${entityName}")`)
-              .html()
-              .replace(
-                `${entityName}`,
-                `<a href="${entityUrl}">${entityName}</a>`
-              ),
-            2000
-          );
+          var userInputArray = userInput.split(" ");
+          userInputArray.map(input => {
+            if (input === entityName) {
+              var updatedArrayItem = `<a href="${entityUrl}" target="_blank">${entityName}</a>`;
+              userInputArray.splice(i, 1, updatedArrayItem);
+              return updatedArrayItem;
+            }
+          });
         }
+        $("#last-definition").html(
+          `<h3 style="color:#fff;">"${userInputArray.join(" ")}"</h3>`
+        );
+        $("#userInput").val("");
       })
       .done(function(data) {
-        console.log("success");
+        //console.log("success");
       })
       .fail(function() {
-        console.log("error");
+        //console.log("error");
       });
 
     //-------------------------------  KeyPhrase API -----------------
@@ -110,7 +108,6 @@ $(function() {
       keyPhrases: keyPhrase,
       text: userInput
     });
-    console.log(userData);
 
     $.ajax({
       url:
@@ -130,10 +127,7 @@ $(function() {
     })
       .then(function(response) {
         var data = response;
-        console.log(data);
-        console.log(data.documents[0].keyPhrases[0]);
         var keyPhrase = data.documents[0].keyPhrases[0];
-        console.log(data);
 
         if (keyPhrase === undefined) {
           $("#key-phrase").html("No Key Phrase");
@@ -149,20 +143,17 @@ $(function() {
       .then(function(response) {
         var data = response;
 
-        console.log(data.documents[0].keyPhrases);
         $(".keyphrase").html(data.documents[0].keyPhrases);
       })
       .done(function(data) {
-        console.log("success");
+        //console.log("success");
       })
       .fail(function() {
-        console.log("error");
+        //console.log("error");
       });
-    $("#userInput").val("");
     $("#searchForm").removeClass("col-md-12");
     $("#searchForm").addClass("col-md-6");
     $("#information").attr("style", "display:block");
-    console.log(keyPhrase);
   });
 
   async function websterAW(keyPhrase) {
@@ -170,7 +161,6 @@ $(function() {
       `https://dictionaryapi.com/api/v3/references/collegiate/json/${keyPhrase}?key=890e17a2-5dcf-4fbe-9e78-69195869c5a2`
     );
     let data = await url.json();
-    console.log(data);
     $("#key-phrase").html(`${keyPhrase}`);
 
     $("#key-phrase-meaning").html(`${data[0].shortdef}`);
@@ -203,7 +193,7 @@ $(function() {
       "load"
     ).innerHTML = `Firebase SDK loaded with ${features.join(", ")}`;
   } catch (e) {
-    console.error(e);
+    //console.error(e);
     document.getElementById("load").innerHTML =
       "Error loading the Firebase SDK, check the console.";
   }
